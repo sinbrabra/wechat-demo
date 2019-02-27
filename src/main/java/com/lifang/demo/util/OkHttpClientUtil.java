ec74aba01951be741b9a3c3f9acc74d08b2d6b36
czq <453528891@qq.com>
2019-02-27 17:40:53 +0800

    http

---
+++ src/main/java/com/lifang/demo/util/OkHttpClientUtil.java
@@ -0,0 +1,13 @@
+package com.lifang.demo.util;
+
+import com.squareup.okhttp.OkHttpClient;
+
+/**
+ * @author czq
+ * @date 2019/2/27
+ */
+public class OkHttpClientUtil {
+
+    private final OkHttpClient client = new OkHttpClient();
+
+}
--- src/main/java/com/lifang/demo/util/OkHttpUtil.java
+++
@@ -1,9 +0,0 @@
-package com.lifang.demo.util;
-
-/**
- * @author czq
- * @date 2019/2/27
- */
-public class OkHttpUtil {
-    
-}
